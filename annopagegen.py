ad9feabd0e0558e8d003cc4e555ebc5b9a13c063
Joshua Phillips <21149530+JoshuaAPhillips@users.noreply.github.com>
2023-05-29 17:04:29 +0100

    fixed dependencies. I think.

--- annopagegen.py
+++ annopagegen.py
@@ -1,54 +1,21 @@
+from bs4 import BeautifulSoup
 import json
+from pprint import pprint as pp
 import xmlparser
 from xmlparser import *
+
+class annoPageGen:
     
-# streams xml from xmlparser()
+  def __init__(self) -> None:
+    filename = xmlparser.filename
+    idno = xmlparser.idno
+    soup = xmlparser.soup
 
-def xmlParser():
-    global idno, xml, facs_list, child_list
 
-    idno = xmlparser.idno.text
-    facs_list = xmlparser.facs_list
-    child_list = xmlparser.child_list
+  
 
-    # print(idno, xml)
+  def test(self):
+    pp(soup)
 
-    return idno, facs_list, child_list
-
-# creates JSON
-
-def toJson(idno, facs_list, child_list):
-
-    global annotation_page
-    annotation_page = {
-        "@context": "http://iiif.io/api/presentation/3/context.json",
-        "id": f"https://raw.githubusercontent.com/JoshuaAPhillips/digital-anon/main/manifests/{idno}-annotations.json",
-        "type": "Manifest",
-        "items": []
-    }
-
-    for i in child_list:
-        counter = 1
-        annotation = {
-            "id": f"https://raw.githubusercontent.com/JoshuaAPhillips/digital-anon/main/manifests/{idno}-annotation-{counter}.json",
-            "type": "Annotation",
-            "motivation": "Commenting",
-            "target": facs_list[counter],
-            "body": {
-                "type": "TextualBody",
-                "language": "en",
-                "format": "text/html",
-                "body": child_list[counter]
-            }
-        }
-        annotation_page["items"].append(annotation)
-        counter += 1
-
-xmlParser()
-toJson(idno, facs_list, child_list)
-
-print(json.dumps(annotation_page, indent=4))
-
-with open (f"{idno}-annotations.json", "w") as json_file:
-    json.dump(annotation_page, json_file, indent=4)
-
+test = annoPageGen()
+test.test()
