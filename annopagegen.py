774ff7432e0e4eed24c65daeaa122a8b778480ad
Joshua Phillips <21149530+JoshuaAPhillips@users.noreply.github.com>
2023-05-31 17:44:57 +0100

    baby steps

--- annopagegen.py
+++ annopagegen.py
@@ -2,3 +2,7 @@ import xml_listgen
 from xml_listgen import *
 import xml.etree.ElementTree as ET
 
+idno = xml_listgen.getIdno()
+div_list = xml_listgen.divList()
+facs_list = xml_listgen.facsList()
+child_list = xml_listgen.childList()
